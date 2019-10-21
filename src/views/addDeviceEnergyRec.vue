d756e2e4f168080d851003346ccefd4b7e0c067d
ideayxg <175@qq.com>
2019-10-21 23:49:54 +0800

    页面修改

--- src/views/addDeviceEnergyRec.vue
+++ src/views/addDeviceEnergyRec.vue
@@ -3,23 +3,23 @@
     <div slot="header" >
       <span>人工录入数据</span>
     </div>
-  <el-container style="margin: 10px;padding: 10px;">
+  <el-container >
 
 
-    <el-aside width="800px" style="padding: 20px;">
+    <el-aside width="700px" type="prev-month" style="padding: 20px;">
 
 
 
       <el-calendar style="border:1px solid #EBEEF5" v-model="dataForm.colDate">
         <!-- 这里使用的是 2.5 slot 语法，对于新项目请使用 2.6 slot 语法-->
-        <template
+        <div @click="clickCalDate(data.day)"
           slot="dateCell"
           slot-scope="{date, data}">
           {{data.day.split('-')[2]}}
           <br><br>
           <i  v-if="1 == 1" class="el-icon-success" style="color: #00a854"></i>
           <i class="el-icon-error" style="color: #ff4679"></i>
-        </template>
+        </div>
 
       </el-calendar>
     </el-aside>
@@ -36,21 +36,27 @@
                 <span style="line-height: 40px;float: left;color: #606266;">采集点名：</span>
               </div>
             </el-col>
-            <el-col :span="4"  >
+            <el-col :span="8"  >
               <div class="grid-content bg-purple">
-                <el-select @change="showUrl" v-model="dataForm.colSettingId" filterable :filter-method="dataFilter"  placeholder="请选择采集点">
+                <el-select  v-model="dataForm.colSettingId" filterable :filter-method="dataFilter"  placeholder="请选择采集点">
                   <span style="font-size: 14px;margin-left: 5px;color: #606266;">采集点名称</span>
                   <span style="float: right; color: #6aa62d; font-size: 13px;margin-right: 5px">采集点ID</span>
                   <hr>
-                  <el-option
-                    v-for="item in options"
-                    :key="item.value"
-                    :label="item.label+'    >    '+item.id"
-                    :value="item.id"
+                  <el-option-group
+                    v-for="group in options"
+                    :key="group.label"
+                    :label="group.label"
                   >
-                    <span style="float: left">{{ item.label }}</span>
-                    <span style="float: right; color: #6aa62d; font-size: 13px">{{ item.id }}</span>
-                  </el-option>
+                    <el-option
+                      v-for="item in group.options"
+                      :key="item.value"
+                      :label="item.label+'    >    '+item.id"
+                      :value="item.id"
+                    >
+                      <span v-bind:class="Math.random() > 0.5 ? 'colsettingname':''" style="float: left">{{ item.label }}</span>
+                      <span style="float: right; color: #6aa62d; font-size: 13px">{{ item.id }}</span>
+                    </el-option>
+                  </el-option-group>
                 </el-select>
               </div>
             </el-col>
@@ -123,25 +129,11 @@
               copy:{},
 
               options: [{
-                  value: '456',
-                  label: '黄金糕',
-                  id : "456"
-              }, {
-                  value: '457',
-                  label: '双皮奶',
-                  id : "457"
-              }, {
-                  value: '458',
-                  label: '蚵仔煎',
-                  id : "458"
-              }, {
-                  value: '459',
-                  label: '龙须面',
-                  id : "459"
-              }, {
-                  value: '选项5',
-                  label: '北京烤鸭',
-                  id : "460"
+                  label: '1号采集器',
+                  options:[{value:'987',label:"黄金点",id:"987"},{value:'988',label:"黄金点2",id:"988"}]
+              },{
+                  label: '2号采集器',
+                  options:[{value:'562',label:"变压器1",id:"562"},{value:'564',label:"变压器2",id:"564"}]
               }],
               storeId:0,
               dataForm:{
@@ -177,14 +169,30 @@
              */
         },
         methods: {
+            disabledDate(time) {
+                /// 不可选天数的设置
+                // return time.getTime() >  new Date(new Date().toLocaleDateString()).getTime();  /// 今天之后不能选择
+                // return time.getTime() > Date.now(); /// 今天之前不可选
+                return time.getTime()   > new Date(new Date().toLocaleDateString()).getTime()-24*60*60*1000;  ///  今天的前一天开始不能选择 （比如今天30号，只能选择到29号，30号后面的都不能选择）
 
-            showUrl() {
-
-
+            },
+            clickCalDate(date){
+                debugger
+              this.$message("改变日历"+date)
+                return true
             },dataFilter(searchData){
                 //对绑定数据赋值
-                this.options = this.copy.filter((item) => {
-                    if((""+item.id).indexOf(searchData) !== -1 || item.label.indexOf(searchData) !== -1) {
+                this.options = JSON.parse(JSON.stringify(this.copy));
+                this.options = this.options.filter((item) => {
+
+                    let subOption = item.options.filter((subItem) => {
+                        if((""+subItem.id).indexOf(searchData) !== -1 || subItem.label.indexOf(searchData) !== -1) {
+                            return true;
+                        }
+
+                    })
+                    item.options = subOption
+                    if(subOption.length !== 0) {
                         return true;
                     }
                 })
@@ -195,5 +203,7 @@
 </script>
 
 <style scoped>
-
+.colsettingname{
+  color: #ff4679;
+}
 </style>
